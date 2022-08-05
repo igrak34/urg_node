f283b8bd8cbcbe0848bed2ce3427d006ec362eb9
Igor Rak <252910@student.pwr.edu.pl>
2022-08-05 12:13:40 +0200

    fix some parameters for namespace usage

--- launch/urg_node_launch.py
+++ launch/urg_node_launch.py
@@ -1,3 +1,4 @@
+from doctest import OutputChecker
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -9,30 +10,63 @@ from launch.actions import SetLaunchConfiguration
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
-
+from launch_ros.actions import PushRosNamespace
+from launch.actions import GroupAction
+import launch_ros
 
 def generate_launch_description():
     urg_node_dir = get_package_share_directory('urg_node')
-    launch_description = LaunchDescription([
-        DeclareLaunchArgument(
-            'sensor_interface',
-            default_value='serial',
-            description='sensor_interface: supported: serial, ethernet')])
+    # launch_description = LaunchDescription([
+    #     DeclareLaunchArgument(
+    #         'sensor_interface',
+    #         default_value='ethernet',
+    #         description='sensor_interface: supported: serial, ethernet')])
 
-    def expand_param_file_name(context):
-        param_file = os.path.join(
-                urg_node_dir, 'launch',
-                'urg_node_' + context.launch_configurations['sensor_interface'] + '.yaml')
-        if os.path.exists(param_file):
-            return [SetLaunchConfiguration('param', param_file)]
+    # def expand_param_file_name(context):
+    #     param_file = os.path.join(
+    #             urg_node_dir, 'launch',
+    #             'urg_node_' + context.launch_configurations['sensor_interface'] + '.yaml')
+    #     if os.path.exists(param_file):
+    #         return [SetLaunchConfiguration('param', param_file)]
 
-    param_file_path = OpaqueFunction(function=expand_param_file_name)
-    launch_description.add_action(param_file_path)
+    # param_file_path = OpaqueFunction(function=expand_param_file_name)
+    # launch_description.add_action(param_file_path)
 
-    hokuyo_node = Node(
-        package='urg_node', executable='urg_node_driver', output='screen',
-        parameters=[LaunchConfiguration('param')]
-        )
+    # hokuyo_node = Node(
+    #     package='urg_node', executable='urg_node_driver', output='screen',
+    #     parameters=[LaunchConfiguration('param')]
+    #     )
 
-    launch_description.add_action(hokuyo_node)
+    # param_file = os.path.join(urg_node_dir,'launch/urg_node_ethernet.yaml')
+
+    # ga = GroupAction(
+    #     actions=[
+    #     PushRosNamespace(namespace='tb6'),
+
+    #     DeclareLaunchArgument(
+    #         'sensor_interface',
+    #         default_value='ethernet',
+    #         description='sensor_interface: supported: serial, ethernet'),
+
+    #     SetLaunchConfiguration('param', param_file),
+
+    #     Node(
+    #         package='urg_node', executable='urg_node_driver', output='screen',
+    #         parameters=[{'ip_adress':'192.168.0.10'}]
+    #     )
+    # ])
+    launch_description = LaunchDescription([])
+    node = Node(
+        package='urg_node', executable='urg_node_driver',output='screen',parameters=[{"ip_adress":"192.168.0.10"},{"ip_port":10940},{"serial_baud":115200}]
+    )
+    ga= GroupAction(
+        actions=[
+            PushRosNamespace(namespace='tb6'),
+
+            Node(
+                package='urg_node', executable='urg_node_driver',output='screen',parameters=[{"ip_adress":"192.168.0.10"},{"ip_port":10940},{"serial_baud":115200}]
+            )
+        ]
+    )
+    launch_description.add_action(ga)
     return launch_description
